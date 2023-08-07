5facbb53056dc2489482330b992d044fe66cf116
Giuseppe Ambrosio <giuambro97@gmail.com>
2023-08-07 19:29:09 +0200

    [ADD] logging conf

--- dagprep/dagprep/__init__.py
+++ dagprep/dagprep/__init__.py
@@ -1,8 +1,12 @@
 import os
 import pathlib as pl
+import logging.config as logging_conf
 
 
 PACKAGE_LOCATION = pl.Path(os.path.dirname(__file__))
 ROOT_LOCATION = PACKAGE_LOCATION.parent
 
-RESOURCE_SOURCE_LOCATION = PACKAGE_LOCATION / "resources"
+RESOURCES_LOCATION = PACKAGE_LOCATION / "resources"
+LOGGING_CONF_LOCATION = RESOURCES_LOCATION / "logging.conf"
+
+logging_conf.fileConfig(LOGGING_CONF_LOCATION)
