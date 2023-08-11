980d02902881fab5150d31a7406b3aa008773423
Giuseppe Ambrosio <giuambro97@gmail.com>
2023-08-11 12:46:38 +0200

    [REFACTOR] minor

--- dagprep/dagprep/pipeline/pipeline.py
+++ dagprep/dagprep/pipeline/pipeline.py
@@ -3,7 +3,7 @@ from typing import Any
 
 from dagprep.pipeline.action_fn import exec, visit
 from dagprep.pipeline.pipeline_explorer import PipelineExplorer
-from dagprep.pipeline.steps.data import DataSource
+from dagprep.pipeline.steps.data_source import DataSource
 
 logger = logging.getLogger(__name__)
 
@@ -11,21 +11,10 @@ logger = logging.getLogger(__name__)
 class Pipeline:
     def __init__(self, data_sources: list[DataSource]):
         self.data_sources = data_sources
+        self.pe = PipelineExplorer(self.data_sources)
 
     def exec(self) -> dict[str, Any]:
-        pe = PipelineExplorer(self.data_sources)
-        outputs = {}
-
-        for tf in self.data_sources:
-            pe.explore(tf, exec, outputs)
-
-        return outputs
+        return self.pe.explore(exec, {})
 
     def get_execution_plan(self) -> list[str]:
-        pe = PipelineExplorer(self.data_sources)
-        topological_order = []
-
-        for tf in self.data_sources:
-            pe.explore(tf, visit, topological_order)
-
-        return topological_order
+        return self.pe.explore(visit, [])
